703fa1bb4f73a129ca640a80676b588a177fbe9f
Camille Sun <camille.sun@Camilles-MacBook-Air.local>
2025-06-23 14:53:47 +0100

    revises handlingFrame, adds two files under revision folder

---
+++ src/main/scala/Revision/HandlingFrame.scala
@@ -0,0 +1,45 @@
+package Week5Day3
+
+import org.openqa.selenium.{By, JavascriptExecutor, WebDriver}
+import org.openqa.selenium.chrome.ChromeDriver
+
+object HandlingFrame extends App {
+
+  val driver:WebDriver = new ChromeDriver()
+  val url = "https://the-internet.herokuapp.com/nested_frames"
+  driver.get(url)
+  val js = driver.asInstanceOf[JavascriptExecutor]
+  val isOnDefaultBefore = js.executeScript("return window.self === window.top").asInstanceOf[Boolean]
+  println(s"isOnDefault: $isOnDefaultBefore")
+  //cant go into any nested frame directly
+  driver.switchTo().frame("frame-top")
+  println("Frame-top found")
+  val isOnDefaultAfter= js.executeScript("return window.self === window.top").asInstanceOf[Boolean]
+  println(s"isOnDefault: $isOnDefaultAfter")
+  driver.switchTo().frame("frame-middle")
+  println("Current Frame: " + driver.findElement(By.tagName("body")).getText)
+  driver.switchTo().defaultContent()
+  val isOnDefaultFinal = js.executeScript("return window.self === window.top").asInstanceOf[Boolean]
+  println(s"isOnDefaultFinal: ${isOnDefaultFinal}")
+  driver.switchTo().frame("frame-bottom")
+  println("Current Frame: " + driver.findElement(By.tagName("body")).getText)
+
+
+//  driver.switchTo().frame("frame-top")
+//  driver.switchTo().frame("frame-middle")
+//  val middleText = driver.findElement(By.id("content")).getText
+//  println("Middle Text: " + middleText)
+//  driver.switchTo().defaultContent()
+//  driver.switchTo().frame(1)
+//  val bottomText = driver.findElement(By.tagName("body")).getText
+//  println("Bottom Text = " + "bottomText")
+//
+//  driver.switchTo().defaultContent()
+//
+//  val frameTop = driver.findElement((By.name("frame-top")))
+//  driver.switchTo().frame(frameTop)
+//  driver.switchTo().frame("frame-left")
+//  val leftText = driver.findElement(By.tagName("body")).getText
+//  println("left text = " + leftText)
+driver.quit()
+}
